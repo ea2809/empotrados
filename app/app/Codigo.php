96aa819c4d5592323665a02aa2b2120fa7f3ea76
Enrique García Galán <ea2809@gmail.com>
2017-03-20 22:28:11 +0100

    Codigos en base de datos sqlite

---
+++ app/app/Codigo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Codigo extends model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code', 'user_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Codigo');
+    }
+}
--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -5,10 +5,14 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Códigos</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    @if($codigo)
+                        Tienes códigos
+                    @else
+                        No tienes códigos
+                    @endif
                 </div>
             </div>
         </div>
